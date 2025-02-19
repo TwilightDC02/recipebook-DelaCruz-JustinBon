12f386eee249c1e2874c84ffbecaf44b85ce05c5
Justin <justinbondc322@gmail.com>
2025-02-19 16:29:24 +0800

    Corrected typos in recipebook and ledger urls.py files

--- ledger/urls.py
+++ ledger/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import recipe_list
 
 urlpatterns = [
-    path('/recipes/list', recipe_list, name="recipe_list"),
+    path('recipes/list', recipe_list, name="recipe_list"),
 ]
 
 app_name = "ledger"

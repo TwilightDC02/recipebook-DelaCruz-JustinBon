9f83b18a3433fc7d60128ed3de92f34493f94685
Justin <justinbondc322@gmail.com>
2025-02-19 16:44:30 +0800

    Added recipe view and dictionaries for recipe1 and recipe2

--- ledger/views.py
+++ ledger/views.py
@@ -68,4 +68,66 @@ def recipe_list(request):
     }
     return render(request, 'recipelist.html', recipelist)
 
+def recipe(request, num):
+    recipe1 = {
+        "name": "Recipe 1",
+        "ingredients": [
+            {
+                "name": "tomato",
+                "quantity": "3pcs"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "pork",
+                "quantity": "1kg"
+            },
+            {
+                "name": "water",
+                "quantity": "1L"
+            },
+            {
+                "name": "sinigang mix",
+                "quantity": "1 packet"
+            }
+        ],
+        "link": "/recipe/1"
+    }
+    recipe2 = {
+        "name": "Recipe 2",
+        "ingredients": [
+            {
+                "name": "garlic",
+                "quantity": "1 head"
+            },
+            {
+                "name": "onion",
+                "quantity": "1pc"
+            },
+            {
+                "name": "vinegar",
+                "quantity": "1/2cup"
+            },
+            {
+                "name": "water",
+                "quantity": "1 cup"
+            },
+            {
+                "name": "salt",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "whole black peppers",
+                "quantity": "1 tablespoon"
+            },
+            {
+                "name": "pork",
+                "quantity": "1 kilo"
+            }
+        ],
+        "link": "/recipe/2"
+    }
+
 # Create your views here.

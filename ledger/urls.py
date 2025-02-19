d66ecc612bb109c4d5807596533609b6a154a649
Justin <justinbondc322@gmail.com>
2025-02-19 14:42:29 +0800

    Created paths for recipe_list and recipe_num and added app_name

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,1 +1,9 @@
-from django.http import HttpResponse
+from django.urls import path
+from .views import home
+
+urlpatterns = [
+    path('/recipes/list', recipe_list, name="recipelist"),
+    path('/recipe/<int:num>', recipe_num, name="recipe_num")
+]
+
+app_name = "ledger"

7b4224f33644279271fd64918f1fcbc9235ca9c0
Justin <justinbondc322@gmail.com>
2025-02-27 19:01:12 +0800

    Created admin classes with customization for each model.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    model = RecipeIngredient
+
+    list_display = ('Recipe', 'Ingredient', 'Quantity')
+    search_fields = ('Recipe',)
+    list_filter = ('Ingredient',)
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)

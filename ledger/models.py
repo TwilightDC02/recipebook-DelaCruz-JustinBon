a3474e3478900e8f2e93fe720a476a601fb27586
Justin <justinbondc322@gmail.com>
2025-02-27 11:49:40 +0800

    Created RecipeIngredient model.

--- ledger/models.py
+++ ledger/models.py
@@ -5,3 +5,8 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+
+class RecipeIngredient(models.Model):
+    Quantity = models.IntegerField()
+    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient')
+    Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe')

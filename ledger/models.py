a9a757ca1244db89c0d44d62c0c4b4c0581e77be
Justin <justinbondc322@gmail.com>
2025-02-27 12:11:33 +0800

    Imported reverse function, and added string and absolute url representations for the Recipe model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return 'This is the recipe for: {}'.format(self.name)
+    
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.name)])
 
 class RecipeIngredient(models.Model):
     Quantity = models.IntegerField()

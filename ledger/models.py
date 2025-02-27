134728189aababd39c19ccd587007acb1a0f1cc0
Justin <justinbondc322@gmail.com>
2025-02-27 11:26:15 +0800

    Created Ingredient and Recipe models.

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)

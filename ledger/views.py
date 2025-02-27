16d1d9d1a33c96892c22d44d97a6f0c7e974e184
Justin <justinbondc322@gmail.com>
2025-02-27 13:44:56 +0800

    Made initial migrations and modified recipe_list view to use Recipe model.

--- ledger/views.py
+++ ledger/views.py
@@ -1,72 +1,10 @@
 from django.shortcuts import render, HttpResponse
 from django.template import loader
+from .models import *
 
 def recipe_list(request):
-    recipelist = {
-        "recipes": [
-        {
-            "name": "Recipe 1",
-            "ingredients": [
-                {
-                    "name": "tomato",
-                    "quantity": "3pcs"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1kg"
-                },
-                {
-                    "name": "water",
-                    "quantity": "1L"
-                },
-                {
-                    "name": "sinigang mix",
-                    "quantity": "1 packet"
-                }
-            ],
-            "link": "/ledger/recipe/1"
-        },
-        {
-            "name": "Recipe 2",
-            "ingredients": [
-                {
-                    "name": "garlic",
-                    "quantity": "1 head"
-                },
-                {
-                    "name": "onion",
-                    "quantity": "1pc"
-                },
-                {
-                    "name": "vinegar",
-                    "quantity": "1/2cup"
-                },
-                {
-                    "name": "water",
-                    "quanity": "1 cup"
-                },
-                {
-                    "name": "salt",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "whole black peppers",
-                    "quantity": "1 tablespoon"
-                },
-                {
-                    "name": "pork",
-                    "quantity": "1 kilo"
-                }
-            ],
-            "link": "/ledger/recipe/2"
-        }
-    ]
-    }
-    return render(request, 'recipe_list.html', recipelist)
+    recipes = Recipe.objects.all()
+    return render(request, 'recipe_list.html', {'recipes': recipes})
 
 def recipe(request, recipe_num):
     recipe1 = {

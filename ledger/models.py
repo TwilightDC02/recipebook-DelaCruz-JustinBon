a8c584976f804e5f4dab4da497ede5b2aa2e17d2
Justin <justinbondc322@gmail.com>
2025-02-28 06:00:58 +0800

    Changed urls to properly redirect to the designated recipe page.

--- ledger/models.py
+++ ledger/models.py
@@ -8,7 +8,7 @@ class Ingredient(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('recipe', args=[str(self.name)])
+        return reverse('ledger:recipe', args=[str(self.name)])
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
@@ -17,9 +17,9 @@ class Recipe(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('recipe', args=[str(self.name)])
+        return reverse('ledger:recipe', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
     Quantity = models.CharField(max_length=255)
-    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredient')
-    Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipe')
+    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')

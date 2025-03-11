a47abf661bd0b2c27cc627cc5b1969fa1220c4aa
Justin <justinbondc322@gmail.com>
2025-03-11 14:04:53 +0800

    Modified Recipe model to match lab specifications.

--- ledger/models.py
+++ ledger/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
 
@@ -10,6 +15,9 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, related_name="author")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -21,9 +29,3 @@ class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=255)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
-    

e0d7e4900337101cf30a6d02ac34d3dfcf06f387
Justin <justinbondc322@gmail.com>
2025-03-11 13:45:04 +0800

    Created Profile model.

--- ledger/models.py
+++ ledger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
@@ -17,6 +18,12 @@ class Recipe(models.Model):
         return reverse('ledger:recipe', args=[str(self.id)])
 
 class RecipeIngredient(models.Model):
-    Quantity = models.CharField(max_length=255)
-    Ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
-    Recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+    quantity = models.CharField(max_length=255)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    bio = models.CharField(max_length=255)
+    

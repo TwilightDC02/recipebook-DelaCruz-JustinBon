7a5e4e9194ed7d57741d71487a615cc60ddce9d6
Justin <justinbondc322@gmail.com>
2025-03-15 12:46:31 +0800

    Changed Login Redirect to go to the Recipe List view.

--- recipebook/settings.py
+++ recipebook/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGOUT_REDIRECT_URL = '/home/'
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = 'ledger:recipe_list'

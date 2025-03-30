a37edaaa8b64f4418d2bd80fc9ec166c346d60bd
Justin <justinbondc322@gmail.com>
2025-03-30 16:30:14 +0800

    Transferred code to handle media to project urls, as keeping it in app urls fails to work.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import recipe_list, detailed_recipe
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
     path('recipe/<int:recipe_num>', detailed_recipe, name="recipe")
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "ledger"

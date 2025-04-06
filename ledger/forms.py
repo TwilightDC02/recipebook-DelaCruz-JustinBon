a3340f5a9cb3fc57e279a5d60d81dd0957bfe1d6
Justin <justinbondc322@gmail.com>
2025-04-06 22:06:06 +0800

    Created RecipeImageForm and RecipeImageFormset to enable user to insert images in the website.

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import Recipe, RecipeIngredient, Ingredient
+from .models import Recipe, RecipeIngredient, Ingredient, RecipeImage
 
 class RecipeForm(forms.ModelForm):
     class Meta:
@@ -17,4 +17,10 @@ class RecipeIngredientForm(forms.ModelForm):
         model = RecipeIngredient
         fields = ['Quantity', 'Ingredient']
 
+class RecipeImageForm(forms.ModelForm):
+    class Meta:
+        model = RecipeImage
+        fields = '__all__'
+
 RecipeIngredientFormset = inlineformset_factory(Recipe, RecipeIngredient, RecipeIngredientForm, can_delete=False)
+RecipeImageFormset = inlineformset_factory(Recipe, RecipeImage, RecipeImageForm, can_delete=False, extra=2)

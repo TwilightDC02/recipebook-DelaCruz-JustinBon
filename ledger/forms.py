cdf19f06dc917cc9f2089df1a6ad58428ecd4fa4
Justin <justinbondc322@gmail.com>
2025-04-06 21:20:02 +0800

    Created Ingredient Form that allows users to create new Ingredients that becomes a new option in the RecipeIngredient formset.

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,16 +1,20 @@
 from django import forms
 from django.forms import inlineformset_factory
-from .models import Recipe, RecipeIngredient
+from .models import Recipe, RecipeIngredient, Ingredient
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ['name']
 
+class IngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = ['name']
+
 class RecipeIngredientForm(forms.ModelForm):
-    ingredient = forms.CharField(max_length=255, required=True)
     class Meta:
         model = RecipeIngredient
-        fields = ['Quantity']
+        fields = ['Quantity', 'Ingredient']
 
 RecipeIngredientFormset = inlineformset_factory(Recipe, RecipeIngredient, RecipeIngredientForm, can_delete=False)

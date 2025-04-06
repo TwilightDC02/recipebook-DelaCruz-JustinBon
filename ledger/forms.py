78ce2f341f8776024c1a345e075d2bce10608353
Justin <justinbondc322@gmail.com>
2025-04-06 17:27:33 +0800

    Created RecipeIngredientForm and RecipeIngredientFormset to handle adding ingredients to Recipe.

--- ledger/forms.py
+++ ledger/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import inlineformset_factory
 from .models import Recipe, RecipeIngredient
 
 class RecipeForm(forms.ModelForm):
@@ -6,7 +7,10 @@ class RecipeForm(forms.ModelForm):
         model = Recipe
         fields = ['name']
 
-class RecipeIngredient(forms.ModelForm):
+class RecipeIngredientForm(forms.ModelForm):
+    ingredient = forms.CharField(max_length=255, required=True)
     class Meta:
         model = RecipeIngredient
-        fields = ['Quantity', 'Ingredient']
+        fields = ['Quantity']
+
+RecipeIngredientFormset = inlineformset_factory(Recipe, RecipeIngredient, RecipeIngredientForm, can_delete=False)

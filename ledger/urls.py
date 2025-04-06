684da514c013b770bf1901980835df8933ad5e6a
Justin <justinbondc322@gmail.com>
2025-04-06 17:29:11 +0800

    Added commas to fix error.

--- ledger/urls.py
+++ ledger/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import recipe_list, detailed_recipe, add_recipe, add_image
+from .views import recipe_list, detailed_recipe, add_recipe
 
 urlpatterns = [
     path('recipes/list', recipe_list, name="recipe_list"),
-    path('recipe/<int:recipe_num>', detailed_recipe, name="recipe")
-    path('recipe/add/', add_recipe, name="new_recipe")
-    path('recipe/<int:recipe_num>', add_image, name="add_image")
+    path('recipe/<int:recipe_num>', detailed_recipe, name="recipe"),
+    path('recipe/add/', add_recipe, name="add_recipe"),
+    # path('recipe/<int:recipe_num>', add_image, name="add_image"),
 ]
 
 app_name = "ledger"

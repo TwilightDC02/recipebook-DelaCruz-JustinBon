5c2f615ee0ab9950c9df31b46a83b8d948179df9
Justin <justinbondc322@gmail.com>
2025-04-06 17:29:35 +0800

    Created add_recipe view.

--- ledger/views.py
+++ ledger/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
-from django.template import loader
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import *
+from .models import Profile, Recipe, RecipeImage
+from .forms import RecipeForm, RecipeIngredientFormset
 
 @login_required
 def recipe_list(request):
@@ -20,3 +20,27 @@ def detailed_recipe(request, recipe_num):
     '''
     recipe = Recipe.objects.get(id=recipe_num)
     return render(request, 'recipe.html', {'recipe': recipe})
+
+@login_required
+def add_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        formset = RecipeIngredientFormset(request.POST)
+
+        if form.is_valid() and formset.is_valid():
+            recipe = form.save(commit=False)
+            recipe.author = request.user.profile
+            form.save()
+            formset.instance = recipe
+            formset.save()
+
+            return redirect('ledger:add_recipe')
+        
+    form = RecipeForm()
+    formset = RecipeIngredientFormset()
+
+    recipes = Recipe.objects.all()
+    print(f"request type: {request.method}")
+    print(recipes)
+
+    return render(request, 'add_recipe.html', {'recipes': recipes, 'add_recipe_form': form, 'ingredients_formset': formset})

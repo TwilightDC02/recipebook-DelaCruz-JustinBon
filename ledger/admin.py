e06b01729131072a465daa1ddf5b5b706f27adb5
Justin <justinbondc322@gmail.com>
2025-04-07 20:39:23 +0800

    Changed import * to specify models imported.

--- ledger/admin.py
+++ ledger/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import Recipe, Ingredient, RecipeIngredient, RecipeImage, Profile
 
 class RecipeIngredientReadOnlyInline(admin.TabularInline):
     model = RecipeIngredient
